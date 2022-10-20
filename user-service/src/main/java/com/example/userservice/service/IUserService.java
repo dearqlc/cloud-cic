d4bdbbb11da5541d61b458d6f568ee26b1a29727
qiulicheng <dearqlc@foxmail.com>
2022-10-20 10:51:35 +0800

    优化

--- user-service/src/main/java/com/example/userservice/service/IUserService.java
+++ user-service/src/main/java/com/example/userservice/service/IUserService.java
@@ -2,8 +2,11 @@ package com.example.userservice.service;
 
 import com.example.userservice.entity.User;
 
+/**
+ * @author QLC
+ */
 public interface IUserService {
 
-    User queryById(Integer id);
+     User queryById(Integer id);
 
 }
